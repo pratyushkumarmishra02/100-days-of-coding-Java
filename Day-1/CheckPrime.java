390599addfe32c8f56e4f0d754bf1456c2d8ebc4
PRATYUSA KUMAR MISHRA <146066300+pratyushkumarmishra02@users.noreply.github.com>
2026-06-28 20:18:40 +0530

    Optimize prime checking logic in CheckPrime.java
    Optimized the prime checking algorithm to reduce iterations and added input validation.

--- Day-1/CheckPrime.java
+++ Day-1/CheckPrime.java
@@ -1,3 +1,21 @@
+/*
+ * 
+ * import java.util.Scanner;
+ * 
+ * public class CheckPrime {
+ * 
+ * public static void main(String[] args) { // TODO Auto-generated method stub
+ * Scanner sc = new Scanner(System.in); System.out.println("Enter a number:");
+ * int n = sc.nextInt(); int i=2; int count=1; while(i<=n) { if(n%i==0) {
+ * count++;
+ * 
+ * } i++; } if(count==2) { System.out.println(n+" is a prime."); }else {
+ * System.out.println(n+" is not a prime."); } }
+ * 
+ * }
+ */
+
+//Optimized approach
 import java.util.Scanner;
 
 public class CheckPrime {
@@ -7,16 +25,21 @@ public class CheckPrime {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number:");
 		int n = sc.nextInt();
+		sc.close();
+		if (n <= 1) {
+            System.out.println(n + " is not a prime.");
+            return;
+        }
 		int i=2;
-		int count=1;
-		while(i<=n) {
+		boolean isPrime=true;
+		while(i<=Math.sqrt(n)) {
 			if(n%i==0) {
-				count++;
-				
+				isPrime=false;
+				break;
 			}
 			i++;
 		}
-		if(count==2) {
+		if(isPrime) {
 			System.out.println(n+" is a prime.");
 		}else {
 			System.out.println(n+" is not a prime.");
