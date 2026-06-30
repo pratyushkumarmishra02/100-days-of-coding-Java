be0fea4aa017a80aeb2c650a8eb2c71c0f7328ea
PRATYUSA KUMAR MISHRA <146066300+pratyushkumarmishra02@users.noreply.github.com>
2026-06-30 23:17:09 +0530

    Refactor LCM calculation using GCD method

--- Day-4/LCMOf2Numbers.java
+++ Day-4/LCMOf2Numbers.java
@@ -5,15 +5,22 @@ public class LCMOf2Numbers {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt(), b = sc.nextInt();
-		int x = a, y = b;
-		while(y != 0){
-		int temp = y;
-		y = x % y;
-		x = temp;
-		}
-		int gcd = x;
-		int lcm = (a * b) / gcd;
+		//LCM(a,b) * GCD(a,b) = a * b;
+		int lcm = (a * b)/GCD(a,b);
 		System.out.println(lcm);
+		
 		}
+	
+	public static int GCD(int a, int b) {
+		int min = Math.min(a, b);
+		int GCD=0;
+		for(int i=min;i>=1;i--) {
+			if(a%i==0 && b%i==0) {
+				GCD=i;
+				break;
+			}
+		}
+		return GCD;
+	}
 
 }
