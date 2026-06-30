a448ed09535c076f5009af3208b8ca36b81128fe
PRATYUSA KUMAR MISHRA <146066300+pratyushkumarmishra02@users.noreply.github.com>
2026-06-30 23:07:30 +0530

    Add another process of  GCD calculation for two numbers
    Implement GCD calculation for two numbers using a loop.

--- Day-4/GCDOf2Numbers.java
+++ Day-4/GCDOf2Numbers.java
@@ -13,3 +13,22 @@ public class GCDOf2Numbers {
 	}
 
 }
+
+//Another process
+public class GCDOf2Numbers {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt(), b = sc.nextInt();
+		int min = Math.min(a, b);
+		int GCD=0;
+		for(int i=min;i>=1;i--) {
+			if(a%i==0 && b%i==0) {
+				GCD=i;
+				break;
+			}
+		}
+		System.out.println(GCD);
+	}
+
+}
